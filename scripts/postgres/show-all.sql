55ccc87797f63663e753a649fe1c7fda72176fdf
willpiam <willdoyle422@gmail.com>
2026-02-08 15:13:31 -0500

    useful script

--- scripts/postgres/show-all.sql
+++ scripts/postgres/show-all.sql
@@ -1,1 +1,11 @@
-select * from identities;
+select
+  identities.fingerprint,
+  name_detail.detail as name,
+  email_detail.detail as email
+from identities
+left join details as name_detail
+  on name_detail.identity_fingerprint = identities.fingerprint
+  and name_detail.path = 'name'
+left join details as email_detail
+  on email_detail.identity_fingerprint = identities.fingerprint
+  and email_detail.path = 'email';
